b534847f68e5688f34f15f44267f5ed46edf408b
jmsikorski <sikorsk4@unlv.nevada.edu>
2016-04-24 08:36:36 -0700

    Update L8E3

--- CpE301L8E3a/CpE301L8E3a/main.c
+++ CpE301L8E3a/CpE301L8E3a/main.c
@@ -26,28 +26,26 @@ int main()
 	TCCR1A|=(0<<COM1A0)|(1<<COM1A1)|(0<<COM1B0)|(0<<COM1B1)|(0<<FOC1A)|(0<<FOC1B)|(1<<WGM11)|(0<<WGM10); //TCCR1A = 0x82
 	TCCR1B|=(0<<ICNC1)|(0<<ICES1)|(1<<WGM13)|(1<<WGM12)|(0<<CS12)|(1<<CS11)|(0<<CS10);
 	//start timer with prescaler 8
-	OCR1A = 2310;
+	OCR1A = 2140;
 	// 2330 upper limit
 	// 630 lower limit
 	while (1)
 	{
-		OCR1A=2320;  //reset
+		OCR1A=2140;  //reset
 		_delay_ms(1000); // 0 to 90
-		OCR1A=1470;
-		_delay_ms(2000); //delay 2 seconds
-		
-		OCR1A=2320;  //reset
+		OCR1A=1280;
+		_delay_ms(2000); //delay 2 seconds		
+		OCR1A=2140;  //reset
 		_delay_ms(1000);
-		OCR1A=951; //move to 35degrees
+		OCR1A=1827; //move to 35degrees
 		_delay_ms(1000);
-		OCR1A=1801; //move to 125degrees
+		OCR1A=997; //move to 125degrees
 		_delay_ms(2000); // delay 2 seconds		
-
-		OCR1A=2320;  //reset
+		OCR1A=2140;  //reset
 		_delay_ms(1000);
-		OCR1A=620; //move to 180 degrees
-		_delay_ms(1000);
-		OCR1A=2320; //move to 0 degrees
+		OCR1A=490; //move to 180 degrees
+		_delay_ms(1000);		
+		OCR1A=2140; //move to 0 degrees
 		_delay_ms(2000); // delay 2 seconds	
 	}
 }
